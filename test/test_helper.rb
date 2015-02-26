e1b57fb50e09c4eef4d77bc9914d7e0e10a0b5f4
Scott Clark <scottclark11@gmail.com>
2015-02-26 14:23:48 -0500

    oops, fixing requires

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,12 @@
 require 'simplecov'
 require 'coveralls'
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'minitest/reporters'
 
 Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
 SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
 SimpleCov.start
-
 Coveralls.wear!
 
 require 'lite_page'
-require 'minitest/spec'
-require 'minitest/autorun'
-require 'minitest/reporters'
