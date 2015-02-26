a0d8c5b7509f5817f4165f4d7325717ab69c4c72
Scott Clark <scottclark11@gmail.com>
2015-02-26 14:22:45 -0500

    try to get coveralls working again

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,13 @@
-require 'coveralls'
-Coveralls.wear!
-
 require 'simplecov'
+require 'coveralls'
+
+Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
+SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
 SimpleCov.start
+
+Coveralls.wear!
 
 require 'lite_page'
 require 'minitest/spec'
 require 'minitest/autorun'
 require 'minitest/reporters'
-
-Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
-SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
